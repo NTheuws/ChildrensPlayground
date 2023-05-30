52ddf788f1ec98945fd3df98b34e920ed805fad9
Niels Theuws <niels.theuws@student.fontys.nl>
2023-05-30 13:57:41 +0200

    Added movieclip hitbox and values to the clip and tickets

--- Assets/Scripts/MovieclipGame.cs
+++ Assets/Scripts/MovieclipGame.cs
@@ -1,20 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.Events;
 using UnityEngine;
 
-public class TicketGame : MonoBehaviour
+public class MovieclipGame : MonoBehaviour
 {
-    private int ticketValue = 1;
+    private int clipValue = -3;
 
     // On contact remove object and add 1 to the score.
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Add point to score.
         GameScoring scoring = (GameScoring)FindObjectOfType(typeof(GameScoring));
-        if (scoring != null && scoring.tag == "Score") 
+        if (scoring != null && scoring.tag == "Score")
         {
-            scoring.PointScored(ticketValue);
+            scoring.PointScored(clipValue);
         }
         // Get rid of object.
         Destroy(this.gameObject);
